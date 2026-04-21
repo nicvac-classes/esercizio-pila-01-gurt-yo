b1295ab369052fbb127c44849c58195197e7f67c
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2026-04-21 07:42:32 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -8,18 +8,97 @@ class Esercizio {
     // Il programma parte con una chiamata a main().
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
+        
         Scanner in = new Scanner( System.in );
+        do {
+            String parola = in.nextLine();
+            for (int i = 0; i<parola.length(); ++i) {
+                pila.push(parola.charAt(i));
+            }
+            String parolaInv;
+            for (int i = 0; i<parola.length();++i) {
+                parolaInv += pila.pop();
+            }
+            if (parola.equalsIgnoreCase(parolaInv)) {
+                System.out.println(parola + " è palindroma");
+            } else {
+                System.out.println(parola + " non é palindroma");
+            }
+        } while (parola.toLowerCase()!="x");   
+    }
+}
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+class Pila<T> {
+    Nodo<T> top;
+    public Pila<T>(){
+        this.top = null;
+    }
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push (T dato) {
+        Nodo <T> nodo = new Nodo<>(dato);
+        if (top==null) {
+            top = nodo;
+        }
+        top.next = head;
+        top = nodo
+    }
+
+    public T pop() {
+        if (top==null) {
+            return NoSuchElementException("Pila vuota");
+        }
+        T dato = top.dato;
+        top = top.next;
+        return dato;
+    }
+
+    public T peek() {
+        if (top==null) {
+            return NoSuchElementException("Pila vuota");
+        }
+        return top.dato;
+    }
+
+    public int size() {
+        Pila<T> pilaB = pila;
+        int cont = 0;
+        try {
+            while(true) {
+                T dato = pilaB.pop();
+                ++cont;
+            }
+        } catch (Exception e) {
+            return cont;
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Pila <T> pilaB = pila;
+        try {
+            while (true) {
+                T dato = pilaB.pop();
+                sb.append(dato);
+                sb.append("-->");
+            }
+        } catch (Exception e) {
+            sb.append("NULL");
+            return sb.toString();
+        }
+    }
+}
+
+class Nodo<T>{
+    T dato;
+    Nodo<T> next;
+
+    public Nodo<T> (T dato) {
+        this.dato = dato;
+        this.next = null
     }
 }
 
